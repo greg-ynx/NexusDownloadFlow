f437ec6b1bc2b3649cb25ae1d0407da3d0061e45
Grégory Ployart <gregory.ployartwork@gmail.com>
2023-10-06 03:04:24 +0200

    reformat

--- config/definitions.py
+++ config/definitions.py
@@ -1,6 +1,5 @@
 import os
 
-
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 MAIN_PATH = os.path.join(ROOT_DIR, 'main.py')
 ASSETS_DIR = os.path.join(ROOT_DIR, 'assets')
