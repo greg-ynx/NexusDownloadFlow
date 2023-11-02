f01bf572abda635f75eaadfc5b28cfeb4bfa9958
Grégory Ployart <gregory.ployartwork@gmail.com>
2023-11-02 03:27:15 +0100

    [0021] [ndf-script] [test] multiscale template matching algorithm demo

--- test/demo/ndf_2.0.0-snapshot_template_matching_demo.py
+++ test/demo/ndf_2.0.0-snapshot_template_matching_demo.py
@@ -1,8 +1,10 @@
 """
-This test file is used to test NexusDownloadFlow's v2.0.0-SNAPSHOT algorithm.
+Test file is used to test NexusDownloadFlow's v2.0.0-SNAPSHOT algorithm.
+
 The algorithm used is the edges template matching and the TM_CCOEFF_NORMED comparison method from OpenCV.
 """
 import os
+import sys
 import time
 
 import cv2
@@ -12,17 +14,16 @@ from mss import mss
 
 from config.definitions import ASSETS_DIRECTORY
 
-
 SCREENSHOT: str = "screenshot.png"
 TEST_TEXT: str = "[TEST] [2.0.0-SNAPSHOT] "
-THRESHOLD: float = .65
+THRESHOLD: float = 0.65
 
 
-def logging_test(text: str) -> None:
-    print(TEST_TEXT + text)
+def _logging_test(text: str) -> None:
+    sys.stdout.write(TEST_TEXT + text + "\n")
 
 
-def load_templates() -> list[MatLike]:
+def _load_templates() -> list[MatLike]:
     return [
         cv2.imread(os.path.join(ASSETS_DIRECTORY, "template1.png")),
         cv2.imread(os.path.join(ASSETS_DIRECTORY, "template2.png")),
@@ -30,13 +31,13 @@ def load_templates() -> list[MatLike]:
     ]
 
 
-def init_templates() -> list[MatLike]:
-    return [cv2.Canny(cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), 50, 200) for template in load_templates()]
+def _init_templates() -> list[MatLike]:
+    return [cv2.Canny(cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), 50, 200) for template in _load_templates()]
 
 
-def test_algorithm() -> None:
-    logging_test("ndf-2.0.0-snapshot-edges-template-matching.")
-    logging_test("Comparaison method: TM_CCOEFF_NORMED.")
+def _test_algorithm() -> None:
+    _logging_test("ndf-2.0.0-snapshot-edges-template-matching.")
+    _logging_test("Comparaison method: TM_CCOEFF_NORMED.")
     try:
         with mss() as mss_instance:
             while True:
@@ -45,11 +46,11 @@ def test_algorithm() -> None:
                 screenshot = cv2.imread(next(mss_instance.save(mon=-1, output=SCREENSHOT)))
                 screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
                 screenshot = cv2.Canny(screenshot, 50, 200)
-                for template in init_templates():
+                for template in _init_templates():
                     match_template = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
                     _, max_value, _, max_location = cv2.minMaxLoc(match_template)
                     if max_value > THRESHOLD:
-                        logging_test("Match found!")
+                        _logging_test("Match found!")
                         match_left_top_location = (
                             max_location[0] + monitors_left_top[0],
                             max_location[1] + monitors_left_top[1],
@@ -63,15 +64,15 @@ def test_algorithm() -> None:
                         time.sleep(6)
                         break
     except SystemExit:
-        logging_test("Exiting the program...")
+        _logging_test("Exiting the program...")
         raise
     finally:
         if os.path.exists(SCREENSHOT):
             os.remove(SCREENSHOT)
         else:
-            logging_test("The file does not exist")
-        logging_test("Program ended")
+            _logging_test("The file does not exist")
+        _logging_test("Program ended")
 
 
 if __name__ == "__main__":
-    test_algorithm()
+    _test_algorithm()
--- test/demo/ndf_2.0.0_template_matching_demo.py
+++ test/demo/ndf_2.0.0_template_matching_demo.py
@@ -1,19 +1,23 @@
 """
-This test file is used to test NexusDownloadFlow's v2.0.0 algorithm.
+Test file is used to test NexusDownloadFlow's v2.0.0 algorithm.
+
 The algorithm used is the multiscale template matching and the TM_CCOEFF_NORMED comparison method from OpenCV.
 """
 import os
-import time
-from typing import Any
+import sys
+from time import sleep
+from typing import Sequence, cast
 
-import cv2
-import pyautogui
+from cv2 import COLOR_BGR2GRAY, TM_CCOEFF_NORMED, Canny, cvtColor, imread, matchTemplate, minMaxLoc, resize
 from cv2.typing import MatLike
 from mss import mss
+from pyautogui import moveTo
 
 from config.definitions import ASSETS_DIRECTORY
 
-CHUNK_SLICES: list[float] = [
+EDGE_MIN_VALUE: int = 50
+EDGE_MAX_VALUE: int = 200
+SCALES: list[float] = [
     1.0,
     0.95789474,
     0.91578947,
@@ -35,72 +39,103 @@ CHUNK_SLICES: list[float] = [
     0.24210526,
     0.2,
 ]
-TEST_TEXT: str = "[TEST] [2.0.0] "
-THRESHOLD: float = .65
 SCREENSHOT: str = "screenshot.png"
+TEMPLATES: list[MatLike] = [
+    imread(os.path.join(ASSETS_DIRECTORY, "template1.png")),
+    imread(os.path.join(ASSETS_DIRECTORY, "template2.png")),
+    imread(os.path.join(ASSETS_DIRECTORY, "template3.png")),
+]
+TEST_TEXT: str = "[TEST] [2.0.0] "
+THRESHOLD: float = 0.65
 
 
-def logging_test(text: str) -> None:
-    print(TEST_TEXT + text)
+def _logging_test(text: str) -> None:
+    sys.stdout.write(TEST_TEXT + text + "\n")
 
 
-def load_templates() -> list[MatLike]:
-    return [
-        cv2.imread(os.path.join(ASSETS_DIRECTORY, "template1.png")),
-        cv2.imread(os.path.join(ASSETS_DIRECTORY, "template2.png")),
-        cv2.imread(os.path.join(ASSETS_DIRECTORY, "template3.png")),
-    ]
+def _init_templates() -> list[MatLike]:
+    return [Canny(cvtColor(template, COLOR_BGR2GRAY), EDGE_MIN_VALUE, EDGE_MAX_VALUE) for template in TEMPLATES]
 
 
-def init_templates() -> list[MatLike]:
-    return [cv2.Canny(cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), 50, 200) for template in load_templates()]
+def _resize_screenshot(screenshot: MatLike, scale: float) -> MatLike:
+    new_width: int = int(screenshot.shape[1] * scale)
+    new_height: int = int(screenshot.shape[0] * scale)
+    return cast(MatLike, resize(screenshot, (new_width, new_height)))
 
 
-def multiscale_template_matching(screenshot: Any, left_top_coordinates: Any) -> None:
-    for template in init_templates():
-        for scale in CHUNK_SLICES:
-            dsize_x, dsize_y = (int(screenshot.shape[1] * scale), int(screenshot.shape[0] * scale))
-            resized_screenshot = cv2.resize(screenshot, (dsize_x, dsize_y))
-            edges = cv2.Canny(resized_screenshot, 50, 200)
-            match_template = cv2.matchTemplate(edges, template, cv2.TM_CCOEFF_NORMED)
-            _, max_value, _, max_location = cv2.minMaxLoc(match_template)
-            if max_value > THRESHOLD:
-                logging_test("Match found!")
-                match_left_top_location = (
-                    max_location[0] + left_top_coordinates[0],
-                    max_location[1] + left_top_coordinates[1],
+def _get_potential_match(screenshot: MatLike, template: MatLike) -> tuple[float, Sequence[int]]:
+    matches: MatLike = matchTemplate(screenshot, template, TM_CCOEFF_NORMED)
+    potential_match: tuple[float, float, Sequence[int], Sequence[int]] = minMaxLoc(matches)
+    max_value: float = potential_match[1]
+    max_location: Sequence[int] = potential_match[3]
+    return max_value, max_location
+
+
+def _if_monitors_left_top_present(monitors_size: dict[str, int]) -> tuple[int, int]:
+    monitors_left: int | None = monitors_size.get("left")
+    monitors_top: int | None = monitors_size.get("top")
+    if monitors_left is None:
+        raise ValueError("monitors_size 'left' value is None")
+    if monitors_top is None:
+        raise ValueError("monitors_size 'top' value is None")
+    return monitors_left, monitors_top
+
+
+def _is_match_found(match_value: float) -> bool:
+    return match_value > THRESHOLD
+
+
+def _multiscale_match_template(
+    templates: list[MatLike], screenshot: MatLike, left_top_coordinates: tuple[int, int]
+) -> None:
+    for scale in SCALES:
+        resized_screenshot: MatLike = _resize_screenshot(screenshot, scale)
+        edged_screenshot: MatLike = Canny(resized_screenshot, 50, 200)
+        for template in templates:
+            potential_match: tuple[float, Sequence[int]] = _get_potential_match(edged_screenshot, template)
+            potential_match_value: float = potential_match[0]
+            potential_match_location: Sequence[int] = potential_match[1]
+            if _is_match_found(potential_match_value):
+                _logging_test("Match found!")
+                match_location_x: int = potential_match_location[0]
+                match_location_y: int = potential_match_location[1]
+                match_left_top_location: tuple[int, int] = (
+                    match_location_x + left_top_coordinates[0],
+                    match_location_y + left_top_coordinates[1],
                 )
-                template_height, template_width = template.shape
-                target = (
+                template_height: int = template.shape[0]
+                template_width: int = template.shape[1]
+                target: tuple[float, float] = (
                     match_left_top_location[0] + template_width / 2,
                     match_left_top_location[1] + template_height / 2,
                 )
-                pyautogui.moveTo(target)
-                time.sleep(6)
+                moveTo(target)
+                sleep(6)
                 return
 
 
-def test_algorithm() -> None:
-    logging_test("ndf-2.0.0-multiscale-template-matching.")
-    logging_test("Comparaison method: TM_CCOEFF_NORMED.")
+def _test_algorithm() -> None:
+    _logging_test("ndf-2.0.0-multiscale-template-matching.")
+    _logging_test("Comparaison method: TM_CCOEFF_NORMED.")
+    edged_templates: list[MatLike] = _init_templates()
     try:
         with mss() as mss_instance:
             while True:
-                monitors_size = mss_instance.monitors[0]
-                monitors_left_top = (monitors_size.get("left"), monitors_size.get("top"))
-                screenshot = cv2.imread(next(mss_instance.save(mon=-1, output=SCREENSHOT)))
-                screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
-                multiscale_template_matching(screenshot, monitors_left_top)
-    except SystemExit:
-        logging_test("Exiting the program...")
-        raise
+                monitors_size: dict[str, int] = mss_instance.monitors[0]
+                monitors_left_top: tuple[int, int] = _if_monitors_left_top_present(monitors_size)
+                screenshot: MatLike = imread(next(mss_instance.save(mon=-1, output=SCREENSHOT)))
+                grayscale_screenshot: MatLike = cvtColor(screenshot, COLOR_BGR2GRAY)
+                _multiscale_match_template(edged_templates, grayscale_screenshot, monitors_left_top)
+    except (SystemExit, KeyboardInterrupt):
+        _logging_test("Exiting the program...")
+        sys.exit(0)
     finally:
         if os.path.exists(SCREENSHOT):
             os.remove(SCREENSHOT)
         else:
-            logging_test("The file does not exist")
-        logging_test("Program ended")
+            _logging_test("The file does not exist")
+        _logging_test("Program ended")
 
 
 if __name__ == "__main__":
-    test_algorithm()
+    _test_algorithm()
